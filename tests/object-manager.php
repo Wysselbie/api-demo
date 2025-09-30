7b3a823769ada3aae9a9d4be26de76d446f6a3d7
Dominik Wißler <wissla1993@aol.com>
2025-09-30 12:26:03 +0200

    fix: Apply cs-fix

--- tests/object-manager.php
+++ tests/object-manager.php
@@ -6,4 +6,5 @@ require __DIR__.'/../tests/bootstrap.php';
 
 $kernel = new Kernel($_SERVER['APP_ENV'], (bool) $_SERVER['APP_DEBUG']);
 $kernel->boot();
+
 return $kernel->getContainer()->get('doctrine')->getManager();
